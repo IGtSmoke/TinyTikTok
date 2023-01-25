f25a994756b8b95bcde0d3a083ac04a6a3723345
Ishirai <kirakiseki@163.com>
2023-01-26 00:06:02 +0800

    feat: add ginPort in config

---
+++ conf/config.go
@@ -0,0 +1,43 @@
+// Package conf contains global configuration variables / Load configuration file to global variables
+package conf
+
+import (
+	"os"
+
+	"github.com/rs/zerolog/log"
+	yaml "gopkg.in/yaml.v3"
+)
+
+// Config Global configuration variables
+type Config struct {
+	DSN                  string `yaml:"dsn"`
+	SnowflakeID          int64  `yaml:"snowflakeId"`
+	BucketName           string `yaml:"bucketName"`
+	MinioEndpoint        string `yaml:"minioEndpoint"`
+	MinioAccessKeyID     string `yaml:"minioAccessKeyId"`
+	MinioSecretAccessKey string `yaml:"minioSecretAccessKey"`
+	RedisAddr            string `yaml:"redisAddr"`
+	RedisPassword        string `yaml:"redisPassword"`
+	RedisDB              int    `yaml:"redisDb"`
+}
+
+// Conf Global configuration variables
+var Conf *Config
+
+// LoadConfig Load configuration file to global variables
+func LoadConfig() error {
+	ymlFile, err := os.ReadFile("./conf/config.yaml")
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to read config file")
+
+		return err
+	}
+
+	if err = yaml.Unmarshal(ymlFile, &Conf); err != nil {
+		log.Error().Err(err).Msg("Failed to unmarshal config file")
+
+		return err
+	}
+
+	return nil
+}
