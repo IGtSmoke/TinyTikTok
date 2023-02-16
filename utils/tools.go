2b42f99917682d7c731ff4b64ed761bec9b221f6
zzg <872011464@qq.com>
2023-02-16 20:43:48 +0800

    使用bcrypt进行用户密码加密

--- utils/tools.go
+++ utils/tools.go
@@ -1,23 +1,9 @@
 package utils
 
 import (
-	"encoding/base64"
 	"errors"
 	"github.com/gin-gonic/gin"
 )
-
-// Base64Encode 加密 []byte -> string
-func Base64Encode(src []byte) string {
-	result := base64.StdEncoding.EncodeToString(src)
-	return result
-}
-
-// Base64Decode 解密 string -> []byte
-func Base64Decode(src string) ([]byte, error) {
-	result, err := base64.StdEncoding.DecodeString(src)
-
-	return result, err
-}
 
 // GetUserIdByMiddleware 通过上下文获取userId
 func GetUserIdByMiddleware(c *gin.Context) (int64, error) {
